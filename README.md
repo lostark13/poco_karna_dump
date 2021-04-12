adc0f957824055ad50fa6aa47334b3030fa243c7
Lostark13 <tushar9915@gmail.com>
2021-04-12 19:12:21 +0000

    surya: MIUI 12.0.6.4

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# qssi-user 11 RKQ1.200826.002 V12.0.6.0.RJGEUXM release-keys
+# qssi-user 11 RKQ1.200826.002 V12.0.6.4.RJGEUXM release-keys
 - manufacturer: xiaomi
 - platform: sm6150
-- codename: karna
+- codename: surya
 - flavor: qssi-user
 - release: 11
 - id: RKQ1.200826.002
-- incremental: V12.0.6.0.RJGEUXM
+- incremental: V12.0.6.4.RJGEUXM
 - tags: release-keys
-- fingerprint: POCO/karna_eea/karna:11/RKQ1.200826.002/V12.0.6.0.RJGEUXM:user/release-keys
-POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.6.0.RJGEUXM:user/release-keys
-POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.6.0.RJGEUXM:user/release-keys
+- fingerprint: POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.6.4.RJGEUXM:user/release-keys
+POCO/karna_eea/karna:11/RKQ1.200826.002/V12.0.6.4.RJGEUXM:user/release-keys
+POCO/surya_eea/surya:11/RKQ1.200826.002/V12.0.6.4.RJGEUXM:user/release-keys
 - is_ab: false
 - brand: POCO
-- branch: qssi-user-11-RKQ1.200826.002-V12.0.6.0.RJGEUXM-release-keys
-- repo: poco_karna_dump
+- branch: qssi-user-11-RKQ1.200826.002-V12.0.6.4.RJGEUXM-release-keys
+- repo: poco_surya_dump
--- vendor/bin/install-recovery.sh
+++ vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:ba5573af40122a0e6dde3f43abc325b7bd7321aa; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:0d8b1bb9284a6df5d6705dcdf4353545d3e08925; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:80c5b490d4731d04fff15488ebb8dba69b663b9f \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:ba5573af40122a0e6dde3f43abc325b7bd7321aa && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:3c8d46f2974839147cbf6847990574f28b83db46 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:0d8b1bb9284a6df5d6705dcdf4353545d3e08925 && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
